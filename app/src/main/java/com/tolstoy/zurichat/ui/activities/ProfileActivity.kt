804118217ad540852caa606e0b540b105b157165
JamesHardey <jamesade646@gmail.com>
2021-09-17 17:47:58 +0100

    initial for myBranch

--- app/src/main/java/com/tolstoy/zurichat/ui/activities/ProfileActivity.kt
+++ app/src/main/java/com/tolstoy/zurichat/ui/activities/ProfileActivity.kt
@@ -1,24 +1,43 @@
 package com.tolstoy.zurichat.ui.activities
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.tolstoy.zurichat.R
 
 class ProfileActivity: AppCompatActivity() {
 
+    private lateinit var savedName : TextView
+    private lateinit var savedAbout : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        savedName = findViewById(R.id.saved_name)
+        savedAbout = findViewById(R.id.saved_about)
+
         val about = findViewById<ImageView>(R.id.edit_about)
         val camera = findViewById<ImageView>(R.id.img_camera)
+        val userName = findViewById<ImageView>(R.id.edit_name)
+        val dialog = CreateDialog(layoutInflater,this)
+
+        val editNameDialog = dialog.createEditNameDialog(savedName)
+        val editAboutDialog = dialog.createEditAboutDialog(savedAbout)
+
+        userName.setOnClickListener {
+            editNameDialog.show()
+        }
 
         about.setOnClickListener {
-            startActivity(Intent(this, ProfileAboutActivity::class.java))
+            editAboutDialog.show()
         }
 
         camera.setOnClickListener {
@@ -30,7 +49,5 @@ class ProfileActivity: AppCompatActivity() {
 
         }
     }
-
-
 
 }
